d070881f6b418b84b287c820fa1637bc4f6e6e0b
Pourya Fahimi <p.f.challenger369@gmail.com>
2025-05-28 19:28:43 +0330

    database stored locally and added docker-compose

--- models/Article.js
+++ models/Article.js
@@ -4,6 +4,7 @@ const articleSchema = new mongoose.Schema(
   {
     title: { type: String, required: true },
     content: { type: String, required: true },
+    userId: { type: String, required: true },
   },
   { timestamps: true }
 );
--- routes/articles.js
+++ routes/articles.js
@@ -17,4 +17,30 @@ router.post("/", async (req, res) => {
   console.log(">> Incoming POST:", req.body);
 });
 
+router.get("/search", async (req, res) => {
+  const query = req.query.q;
+  try {
+    const results = await Article.find({
+      title: { $regex: query, $options: "i" }, // case-insensitive search
+    });
+    res.json(results);
+  } catch (err) {
+    res.status(500).json({ error: "Search error" });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  // update an article by Id
+  try {
+    const updated = await Article.findByIdAndUpdate(
+      req.params.id,
+      { title: req.body.title, content: req.body.content },
+      { new: true }
+    );
+    res.json(updated);
+  } catch (err) {
+    res.status(500).json({ error: "Update error" });
+  }
+});
+
 module.exports = router;
